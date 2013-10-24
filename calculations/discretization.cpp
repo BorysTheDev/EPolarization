23dd6d0c46015bb0fbe2c7510d881d31bf6da86d
boris <boris@kogorta.dp.ua>
2013-10-24 15:56:10 +0300

    fix bugs and refactoring

--- calculations/discretization.cpp
+++ calculations/discretization.cpp
@@ -1,25 +1,21 @@
 #include "discretization.h"
 
-
 Discretization::Discretization(const CurvesList& sCurves,
-		const IncidentFieldsList& fields) : fields(fields), curves(sCurves) {
-	waveNumber = fields.waveNumber();
-	size = 0;
-	for (size_t i =0; i < sCurves.size(); i++)
+		const IncidentFields& fields) : fields(fields), curves(sCurves),
+		wN(fields.waveNumber()), borders(sCurves.size())
+{
+	for (size_t i = 0; i < sCurves.size(); i++) {
 		size += sCurves[i].size();
+		borders[i] = size;
+	}
 }
 
 MatrixPtr<types::complex> Discretization::createMatrix() {
 	Matrix<types::complex> *matrix = new Matrix<types::complex>(size);
-	size_t startI = 0;
 	for (size_t m = 0; m < curves.size(); m++) {
-		size_t startJ = 0;
     for (size_t n = 0; n < curves.size(); n++) {
-      fillMatrixBlock(*matrix, startI, startJ,
-          curves[m], curves[n], waveNumber);
-      startJ += curves[n].size();
+      fillMatrixBlock(*matrix, m, n);
 		}
-		startI += curves[m].size();
 	}
 	return MatrixPtr<types::complex>(matrix);
 }
@@ -29,31 +25,32 @@ ArrayPtr<types::complex> Discretization::createArray() {
 	int ii = 0;
 	for (size_t i = 0; i < curves.size(); i++) {
 		for (size_t j = 0; j < curves[i].size(); j++, ii++)
-			(*f)[ii] = -fields[0](curves[i][j].x, curves[i][j].y);
+			(*f)[ii] = -fields(curves[i][j].x, curves[i][j].y);
 	}
 	return ArrayPtr<types::complex>(f);
 }
 
 void Discretization::fillMatrixBlock(Matrix<types::complex>& matr,
-    size_t i, size_t j, const DiscretizeCurve& c1,
-    const DiscretizeCurve& c2, types::real waveNumber) {
-  if (i != j) {
+    size_t ci1, size_t ci2) {
+
+  const DiscretizeCurve& c1 = curves[ci1];
+  const DiscretizeCurve& c2 = curves[ci2];
+
+  if (ci1 != ci2) {
     //not diagonal blocks
-    for (size_t ii = 0; ii < c1.size(); ii++) {
-      for (size_t jj = 0; jj < c2.size(); jj++) {
-        matr[i + ii][j + jj] = (M_PI / c2.size())
-            * h2(waveNumber * dist(c1[ii] ,c2[jj]));
+    for (int ii = leftBorderOf(ci1); ii < borders[ci1]; ii++) {
+      for (int jj = leftBorderOf(ci2); jj < borders[ci2]; jj++) {
+        matr[ii][jj] = (M_PI / c2.size()) * h2(wN * dist(c1[ii] ,c2[jj]));
       }
     }
   } else {
     //diagonal block
-    for (size_t ii = 0; ii < c1.size(); ii++) {
-      for (size_t jj = 0; jj < c1.size(); jj++) {
-    	  types::complex temp = ii != jj ? h2(waveNumber * dist(c1[ii], c2[jj]))
-          - epol::asymp(c1[ii].t, c1[jj].t) : epol::lim(c1[ii].d, waveNumber);
-        matr[i + ii][j + jj] = (M_PI / c1.size())
-          * (temp - types::complex(0, 2) * epol::Ln(c1[ii].t, c1[jj].t, c1.size())
-                / M_PI);
+    for (int ii = leftBorderOf(ci1); ii < borders[ci1]; ii++) {
+      for (int jj = leftBorderOf(ci2); jj < borders[ci2]; jj++) {
+    	  types::complex temp = ii != jj ? h2(wN * dist(c1[ii], c2[jj]))
+          - epol::asymp(c1[ii].t, c1[jj].t) : epol::lim(c1[ii].d, wN);
+        matr[ii][jj] = (M_PI / c1.size()) * (temp - types::complex(0, 2) *
+            epol::Ln(c1[ii].t, c1[jj].t, c1.size()) / M_PI);
       }
     }
   }
